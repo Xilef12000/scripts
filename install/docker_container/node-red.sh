217892dd624cff59fc00d3eee184a9de68e1619b
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-02-21 19:53:57 +0100

    Update node-red.sh
    run in background

--- install/docker_container/node-red.sh
+++ install/docker_container/node-red.sh
@@ -10,4 +10,4 @@ chmod -R 777 node-red
 # use nodered/node-red and not nodered/node-red-docker
 
 # port [host]:[application]
-docker run -it -p 81:1880 -v $PWD/node-red/data:/data --restart=unless-stopped --name node-red nodered/node-red
+docker run -d -it -p 81:1880 -v $PWD/node-red/data:/data --restart=unless-stopped --name node-red nodered/node-red
