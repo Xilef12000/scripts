d858b388fa6a4c893548ae0ac8d9f8343227f088
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-03-05 21:42:13 +0100

    openvpn installation
    install openvpn (not the official way)
    https://github.com/Nyr/openvpn-install

---
+++ install/openvpn.sh
@@ -0,0 +1,5 @@
+# install openvpn (not the official way)
+# https://github.com/Nyr/openvpn-install
+# https://www.cyberciti.biz/faq/howto-setup-openvpn-server-on-ubuntu-linux-14-04-or-16-04-lts/
+wget https://git.io/vpn -O openvpn-install.sh
+bash openvpn-install.sh
