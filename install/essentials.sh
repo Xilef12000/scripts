3878000e0e0a77c79fc3c693a696b622753b6cf9
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-04-25 11:17:14 +0200

    added traceroute to essentials

--- install/essentials.sh
+++ install/essentials.sh
@@ -1,6 +1,8 @@
 # install essential tools
 sudo apt update
+sudo apt-get update
 sudo apt install nano -y
 sudo apt install git -y
 sudo apt install neofetch -y
+sudo apt-get install traceroute -y
 
