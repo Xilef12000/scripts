333876b4e2f27590e35e24698fd5ec9734d35b43
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-04-28 10:34:44 +0200

    Create yt-dlg.sh

---
+++ install/yt-dlg.sh
@@ -0,0 +1,8 @@
+# yt-dlg / youtube-dl-gui
+# https://github.com/oleksis/youtube-dl-gui
+# https://snapcraft.io/yt-dlg
+# https://www.filebot.net/forums/viewtopic.php?t=5139
+
+sudo snap install yt-dlg
+# allow access to /mnt and /media directories
+sudo snap connect yt-dlg:removable-media
