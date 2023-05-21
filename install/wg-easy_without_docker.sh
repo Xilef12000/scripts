c4b963c0bc33b81246cd4d4ddf706b89e2c43bf7
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-21 18:02:59 +0200

    Create wg-easy_without_docker.sh

---
+++ install/wg-easy_without_docker.sh
@@ -0,0 +1,23 @@
+# wg-easy without docker
+# https://github.com/wg-easy/wg-easy/wiki/Using-WireGuard-Easy-without-Docker
+
+apt update
+apt install wireguard git nodejs npm ufw wget nano -y
+
+echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+echo net.ipv4.conf.all.src_valid_mark=1 >> /etc/sysctl.conf
+sysctl -p
+cd ~
+git clone https://github.com/WeeJeWel/wg-easy
+cd wg-easy
+mv src /app
+cd /app
+npm ci --production
+cp node_modules ..
+ufw allow 51821/tcp # (webui) Only for users of the UFW firewall
+ufw allow 51820/udp # (wireguard listening port) Only for users of the UFW firewall
+cd -
+nano /etc/systemd/system/wg-easy.service # Replace everything that is marked as 'REPLACEME' and tweak it to your liking
+systemctl daemon-reload
+systemctl enable --now wg-easy.service
+systemctl start wg-easy.service
