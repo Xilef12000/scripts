d56d6e3684e99cd2f5acf80796ab47bc0baabcdd
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-22 18:49:46 +0200

    added wireguard

---
+++ install/wireguard.sh
@@ -0,0 +1,7 @@
+# WireGuard using angristans wireguard-install
+# https://github.com/angristan/wireguard-install
+# https://stanislas.blog/2019/01/how-to-setup-vpn-server-wireguard-nat-ipv6/
+
+curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+chmod +x wireguard-install.sh
+./wireguard-install.sh
