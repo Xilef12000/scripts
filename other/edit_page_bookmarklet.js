10c4113129a7d1a7e2555bb8cac2c19aad032487
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-06-07 22:37:05 +0200

    Create edit_page_bookmarklet.js

---
+++ other/edit_page_bookmarklet.js
@@ -0,0 +1,6 @@
+// Edit-Page Bookmarklet
+//
+// add the following line as bookmark
+// and click on the bookmark to activate editing
+
+javascript:document.body.contentEditable = true; document.designMode = 'on'; void 0;
