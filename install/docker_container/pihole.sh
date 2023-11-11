8fd3009632e1cdebcadf5edae02618af0fd80fb8
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-11-11 19:38:45 +0100

    Update pihole.sh

--- install/docker_container/pihole.sh
+++ install/docker_container/pihole.sh
@@ -1,41 +1,12 @@
 # Pihole - Docker
-# https://www.blog.berrybase.de/blog/2022/04/29/pi-hole-raspberry-pi-docker/
-# https://hub.docker.com/r/pihole/pihole
+# https://github.com/pi-hole/docker-pi-hole/blob/master/examples/docker_run.sh
 
-mkdir pihole
-
-# replace [passwd] with your desired poassword
-# Port [host]:[application]
-cat <<< '
-version: "3"
-
-# replace [passwd] with your desired poassword
-# Port [host]:[application]
-
-# More info at https://github.com/pi-hole/docker-pi-hole/ and https://docs.pi-hole.net/
-services:
-  pihole:
-    container_name: pihole
-    image: pihole/pihole:latest
-    # For DHCP it is recommended to remove these ports and instead add: network_mode: "host"
-    ports:
-      - "53:53/tcp"
-      - "53:53/udp"
-      - "67:67/udp" # Only required if you are using Pi-hole as your DHCP server
-      - "83:80/tcp"
-    environment:
-      TZ: "Germany/Berlin"
-      WEBPASSWORD: "[passwd]"
-    # Volumes store your data between container upgrades
-    volumes:
-      - "./etc:/etc/pihole"
-      - "./etc/dnsmasq.d:/etc/dnsmasq.d"
-    #   https://github.com/pi-hole/docker-pi-hole#note-on-capabilities
-    cap_add:
-      - NET_ADMIN # Required if you are using Pi-hole as your DHCP server, else not needed
-    restart: unless-stopped
-' > pihole/docker-compose.yml
-
-cd pihole
-
-docker-compose up -d
+docker run -d \
+    --name pihole \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 80:80 \
+    -e TZ="Germany/Berlin" \
+    -v "[your\pihole\dir]/etc/pihole:/etc/pihole" \
+    -v "[your\pihole\dir]/etc/dnsmasq.d:/etc/dnsmasq.d" \
+    --restart=unless-stopped \
+    pihole/pihole:latest
