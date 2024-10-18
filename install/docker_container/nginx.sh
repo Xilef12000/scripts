3cde4c1610cb0861e12e59ba279dda8de19c2679
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2024-10-18 18:27:35 +0200

    added path variable

--- install/docker_container/nginx.sh
+++ install/docker_container/nginx.sh
@@ -3,11 +3,15 @@
 # https://www.docker.com/blog/how-to-use-the-official-nginx-docker-image/
 # https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/How-to-setup-Nginx-reverse-proxy-servers-by-example
 
+path=[/your/storage/path]
+
+cd $path
+
 docker pull nginx
 docker run -it --rm -d -p 80:80 --name nginx nginx
-mkdir nginx
-docker cp nginx:/etc/nginx ~/nginx/etc
-docker cp nginx:/usr/share/nginx ~/nginx/share
+
+docker cp nginx:/etc/nginx $path/etc
+docker cp nginx:/usr/share/nginx $path/share
 docker stop nginx
 # docker run -it --rm -d -p 80:80 -v ~/nginx/etc:/etc/nginx -v ~/nginx/share:/usr/share/nginx/ --name nginx nginx
-docker run -it -d -p 80:80 -v ~/nginx/etc:/etc/nginx -v ~/nginx/share:/usr/share/nginx/ --restart=unless-stopped --name nginx nginx
+docker run -it -d -p 80:80 -v $path/etc:/etc/nginx -v $path/share:/usr/share/nginx/ --restart=unless-stopped --name nginx nginx
